e2e7bc61246135fce5eff6571c2cd636eadcd5aa
= <=>
2023-12-29 20:10:11 +0100

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const prod = process.env.NODE_ENV === 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : prod ? '/Seraph/' : '/',
 })
